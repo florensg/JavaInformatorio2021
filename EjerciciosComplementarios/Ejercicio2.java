3bca969d47a496699e38d39a645ecdcc4dcde566
florensg <floo.g1096@gmail.com>
2021-06-07 23:03:36 -0300

    carga ejercicio 2 level 1

---
+++ EjerciciosComplementarios/Ejercicio2.java
@@ -0,0 +1,32 @@
+package JavaInformatorio2021.EjerciciosComplementarios;
+import java.util.Scanner;
+public class Ejercicio2 {
+    public static void main(String[] args){
+        System.out.print("ingrese un numero: ");
+        Scanner scan = new Scanner(System.in);
+        int numero1 = scan.nextInt();
+        System.out.print("Ingrese otro numero: ");
+        int numero2 = scan.nextInt();
+        System.out.println("suma:   " + numero1 + " + " + numero2 + " = " + suma(numero1,numero2));
+        System.out.println("resta:  " + numero1 + " - " + numero2 + " = " + resta(numero1,numero2));
+        System.out.println("multiplicacion:     " + numero1 + " * " + numero2 + " = " + multiplicacion(numero1,numero2));
+        System.out.println("division:   " + numero1 + " / " + numero2 + " = " + division(numero1,numero2));
+        System.out.println("modulo:     " + numero1 + " % " + numero2 + " = " + modulo(numero1,numero2));
+        scan.close();
+    }
+    public static int suma(int num1,int num2){
+        return num1 + num2;
+    };
+    public static int resta(int num1,int num2){
+        return num1 - num2;
+    };
+    public static int multiplicacion(int num1,int num2){
+        return num1 * num2;
+    };
+    public static int division(int num1,int num2){
+        return num1 / num2;
+    };
+    public static int modulo(int num1,int num2){
+        return num1 % num2;
+    };
+}

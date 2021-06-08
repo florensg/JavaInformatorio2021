1c0804c3b1a47fe522d668fae0356fd7c4d24e6e
florensg <floo.g1096@gmail.com>
2021-06-07 22:02:17 -0300

    carga ejercicio 1 level 1

---
+++ EjerciciosComplementarios/Ejercicio1.java
@@ -0,0 +1,13 @@
+package JavaInformatorio2021.EjerciciosComplementarios;
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args){
+        System.out.println("ingrese su nombre");
+        String nombre = "";
+        Scanner nombreEntrada = new Scanner(System.in);
+        nombre = nombreEntrada.nextLine();
+        System.out.println("HOLA "+nombre+"!!!"); 
+        nombreEntrada.close();
+    }
+}
